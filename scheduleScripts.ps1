3f851fd4256a85d492a72640b09700572c4549bd
adomce123 <adomcem@gmail.com>
2020-02-04 12:12:50 +0200

    Delete scheduleScripts.ps1

--- scheduleScripts.ps1
+++
@@ -1,2 +0,0 @@
-﻿SCHTASKS /CREATE /SC ONCE /TN "MICROSOFT\BiggestLowest6" /TR "C:\Users\bd6612\Scripts\grafana_sound.py" /ST 12:11 /RU "BD6612" /RL HIGHEST
-SCHTASKS /QUERY /TN "MICROSOFT\BiggestLowest6"
